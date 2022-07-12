9681eb026760eebeb280a457ad58004827fd9696
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-07-12 15:24:36 +0000

    emit & event listener

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,64 +1,59 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList, AssignmentCreate },
 
     template: `
     <section class="space-y-6">
         <assignment-list :assignments="inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="completed" title="Completed"></assignment-list>
         
-        <form @submit.prevent="add">
-            <div class="border border-gray-500 rounded-sm text-black">
-                <input v-model="newAssignment" placeholder="New task..." class="p-2"/>
-                <button type="submit" class="bg-white p-2 border-l text-bold">+</button>
-            </div>
-        </form>
+        <assignment-create @add="add"></assignment-create>
     </section>
 `,
 
     data() {
-    return {
-        assignments: [
-            {
-                name: 'Finish Tutorial',
-                complete: false,
-                id: 1
-            },
-            {
-                name: 'Understand Vue 3',
-                complete: false,
-                id: 2
-            },
-            {
-                name: 'Build something with it',
-                complete: false,
-                id: 3
-            }
-        ],
-        newAssignment: ''
-    };
-},
-
-computed: {
-    inProgress() {
-        return this.assignments.filter(a => !a.complete);
+        return {
+            assignments: [
+                {
+                    name: 'Finish Tutorial',
+                    complete: false,
+                    id: 1
+                },
+                {
+                    name: 'Understand Vue 3',
+                    complete: false,
+                    id: 2
+                },
+                {
+                    name: 'Build something with it',
+                    complete: false,
+                    id: 3
+                }
+            ]
+        };
     },
 
-    completed() {
-        return this.assignments.filter(a => a.complete);
-    }
-},
+    computed: {
+        inProgress() {
+            return this.assignments.filter(a => !a.complete);
+        },
 
-methods: {
-    add() {
-        this.assignments.push({
-            name: this.newAssignment,
-            completed: false,
-            id: this.assignments.length + 1
-        });
+        completed() {
+            return this.assignments.filter(a => a.complete);
+        }
+    },
 
-        this.newAssignment = '';
+    methods: {
+        add(name) {
+            this.assignments.push({
+                name: name,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
+        }
     }
-}
 }
