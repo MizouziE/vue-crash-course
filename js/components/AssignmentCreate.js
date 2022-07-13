afb3b4ba47ce28bbc032eb276f85183efc673b07
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-07-13 13:15:15 +0000

    + button fit inside input

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -1,8 +1,8 @@
 export default {
     template: `
         <form @submit.prevent="add">
-            <div class="border border-gray-500 rounded-sm text-black flex">
-                <input v-model="newAssignment" placeholder="New task..." class="p-2"/>
+            <div class="border border-gray-500 text-black flex">
+                <input v-model="newAssignment" placeholder="New task..." class="p-2 -mr-6"/>
                 <button type="submit" class="bg-white p-2 border-l text-bold">+</button>
             </div>
         </form>
